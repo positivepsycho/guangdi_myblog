62352200f9885722bb6c4ecb9eb19394eabee253
positivepsycho <dayangbianx@gamil.com>
2019-07-07 10:37:02 +0800

    change info

--- source/conf.py
+++ source/conf.py
@@ -43,8 +43,8 @@ source_suffix = ['.rst','.md']
 master_doc = 'index'
 
 # General information about the project.
-project = u'MING\'s BLOG' 
-copyright = u'2018, 小明同学'
+project = u'广砥\'的-博客' 
+copyright = u'2019, data'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -92,8 +92,8 @@ latex_elements={# The paper size ('letterpaper' or 'a4paper').
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'python3-cookbook.tex', u'《Python编程时光》',
-   u'小明同学', 'howto'),
+  ('index', 'python3-cookbook.tex', u'TRY_MORE',
+   u'GD', 'howto'),
 ]
 
 # -- Options for manual page output ---------------------------------------
@@ -101,8 +101,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'python3-cookbook', u'《Python编程时光》',
-     [u'小明同学'], 1)
+    ('index', 'python3-cookbook', u'TRY_MORE',
+     [u'GD'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -115,8 +115,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Python-Time', u'《Python编程时光》',
-   u'小明同学', 'Python-Time', '《Python编程时光》',
+  ('index', 'Python-Time', u'TRY_MORE',
+   u'GD', 'Python-Time', 'TRY_MORE',
    'Miscellaneous'),
 ]
 
--- source/index.rst
+++ source/index.rst
@@ -1,9 +1,9 @@
-.. MING's BLOG documentation master file, created by
+.. LGD's BLOG documentation master file, created by
    sphinx-quickstart on Sat Jun 23 16:57:48 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to MING's BLOG's documentation!
+Welcome to LGD's BLOG's documentation!
 =======================================
 
 .. toctree::
@@ -11,5 +11,6 @@ Welcome to MING's BLOG's documentation!
    :caption: Contents:
    
    how_to_be_a_rich_man
+   
    blog writing
 
